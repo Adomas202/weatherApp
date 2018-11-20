eb80c0bf03db339d9505bfc9b21797ebcacfd4d3
Adomas Poleninas <adomas.poleninas@gmail.com>
2018-11-21 00:47:29 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import SearchBar from './components/SearchBox';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+          <SearchBar/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const searchBar = () => {
+    return(
+        <div><input type="text"/></div>
+    )
+};
+
+export default searchBar;
