3c3c8c932249a360f9ac3a69994b144b368cf628
Adomas Poleninas <adomas.poleninas@gmail.com>
2018-12-08 17:51:46 +0200

    Fix validation with suggestions

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -19,13 +19,13 @@ class searchForm extends Component {
     handleValidation = () => {
         let inputError = "";
 
-        if (this.state.input.length === 0) {
+        if (this.state.address.length === 0) {
             inputError = "The input field can't be emtpy";
             this.setState({inputError});
             return false;
         }
 
-        if (!this.state.input.match(/^[0-9a-zA-Z]+$/)) {
+        if (!this.state.address.match(/^[0-9a-zA-Z]+$/)) {
             inputError = "You can't enter non alphanumeric characters";
             this.setState({inputError});
             return false;
@@ -36,14 +36,15 @@ class searchForm extends Component {
 
     handleSubmit = event => {
         event.preventDefault();
+    };
+
+    handleSelect = address => {
         const isValid = this.handleValidation();
+        console.log(isValid);
         if (isValid) {
             this.setState({inputError: ""});
             this.props.handleFromParent(this.state.input);
         }
-    };
-
-    handleSelect = address => {
         geocodeByAddress(address)
             .then(results => getLatLng(results[0]))
             .then(latLng => {
@@ -58,50 +59,51 @@ class searchForm extends Component {
 
     render() {
         return (
-            <PlacesAutocomplete
-                value={this.state.address}
-                onChange={this.handleChange}
-                onSelect={this.handleSelect}
-            >
-                {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
-                    <div>
-                        <input
-                            {...getInputProps({
-                                placeholder: 'Search Places ...',
-                                className: 'location-search-input',
-                            })}
-                        />
-                        <div className="autocomplete-dropdown-container">
-                            {loading && <div>Loading...</div>}
-                            {suggestions.map(suggestion => {
-                                const className = suggestion.active
-                                    ? 'suggestion-item--active'
-                                    : 'suggestion-item';
-                                // inline style for demonstration purpose
-                                const style = suggestion.active
-                                    ? {backgroundColor: '#fafafa', cursor: 'pointer'}
-                                    : {backgroundColor: '#ffffff', cursor: 'pointer'};
-                                return (
-                                    <div
-                                        {...getSuggestionItemProps(suggestion, {
-                                            className,
-                                            style,
-                                        })}
-                                    >
-                                        <span>{suggestion.description}</span>
-                                    </div>
-                                );
-                            })}
-                        </div>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <PlacesAutocomplete
+                        value={this.state.address}
+                        onChange={this.handleChange}
+                        onSelect={this.handleSelect}
+                    >
+                        {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
+                            <div>
+                                <input
+                                    {...getInputProps({
+                                        placeholder: 'Search for preffered location...',
+                                        className: 'location-search-input',
+                                    })}
+                                />
+                                <div className="autocomplete-dropdown-container">
+                                    {loading && <div>Loading...</div>}
+                                    {suggestions.map(suggestion => {
+                                        const className = suggestion.active
+                                            ? 'suggestion-item--active'
+                                            : 'suggestion-item';
+                                        // inline style for demonstration purpose
+                                        const style = suggestion.active
+                                            ? {backgroundColor: '#fafafa', cursor: 'pointer'}
+                                            : {backgroundColor: '#ffffff', cursor: 'pointer'};
+                                        return (
+                                            <div
+                                                {...getSuggestionItemProps(suggestion, {
+                                                    className,
+                                                    style,
+                                                })}
+                                            >
+                                                <span>{suggestion.description}</span>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        )}
+                    </PlacesAutocomplete>
+                    <div className="input--error">
+                        {this.state.inputError}
                     </div>
-                )}
-            </PlacesAutocomplete>
-            // <form onSubmit={this.handleSubmit}>
-            //     <input type='text' placeholder='Enter City Name...' onKeyDown={this.handleChange}/>
-            //     <div className="input--error">
-            //         {this.state.inputError}
-            //     </div>
-            // </form>
+                </form>
+            </div>
         )
     }
 }
